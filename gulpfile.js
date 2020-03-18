9ee5aaada39f759f86da1fc409c6f3c442336c55
Yurii Khomenko <yuriikhomenko@macBookProYurii.home>
2020-03-18 20:53:43 +0100

    Adds solution to task #1

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,12 @@
-const gulp = require('gulp');
-const sass = require('gulp-sass');
-const sasslint = require('gulp-sass-lint');
-const browserSync = require('browser-sync').create();
-const uglify = require('gulp-uglify');
-const concat = require('gulp-concat');
-const babel = require('gulp-babel');
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var sasslint = require('gulp-sass-lint');
+var browserSync = require('browser-sync').create();
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
+var babel = require('gulp-babel');
 
-gulp.task('browserSync', function() {
+gulp.task('browserSync', function () {
   browserSync.init({
     server: {
       watch: true,
@@ -15,7 +15,7 @@ gulp.task('browserSync', function() {
   })
 });
 
-gulp.task('sass-lint', function() {
+gulp.task('sass-lint', function () {
   return gulp.src(['src/scss/**/*.scss'])
     .pipe(sasslint())
     .pipe(sasslint())
@@ -23,18 +23,18 @@ gulp.task('sass-lint', function() {
     .pipe(sasslint.failOnError())
 });
 
-gulp.task('sass', function() {
+gulp.task('sass', function () {
   return gulp.src('src/scss/styles.scss')
-    .pipe(sass({outputStyle: 'compressed'}))
+    .pipe(sass({ outputStyle: 'compressed' }))
     .pipe(concat('styles.min.css'))
     .pipe(gulp.dest('dist/css'))
     .pipe(browserSync.reload({
-        stream: true
-      }
+      stream: true
+    }
     ))
 });
 
-gulp.task('js', function() {
+gulp.task('js', function () {
   return gulp.src('src/js/*.js')
     .pipe(babel({
       presets: ['@babel/env']
@@ -43,31 +43,31 @@ gulp.task('js', function() {
     .pipe(uglify())
     .pipe(gulp.dest('dist/js'))
     .pipe(browserSync.reload({
-        stream: true
-      }
+      stream: true
+    }
     ))
 });
 
-gulp.task('html', function() {
+gulp.task('html', function () {
   return gulp.src('src/*.html')
     .pipe(gulp.dest('dist'))
     .pipe(browserSync.reload({
-        stream: true
-      }
+      stream: true
+    }
     ))
 });
 
-gulp.task('fonts', function() {
+gulp.task('fonts', function () {
   return gulp.src('src/fonts/**/*.ttf')
     .pipe(gulp.dest('dist/fonts'));
 });
 
 gulp.task('images', function () {
-  return gulp.src('src/images/**/*')
-    .pipe(gulp.dest('dist/images'))
+  return gulp.src('src/img/**/*')
+    .pipe(gulp.dest('dist/img'))
 });
 
-gulp.task('watch', function(){
+gulp.task('watch', function () {
   gulp.watch(['src/js/**/*.js', 'src/*.html', 'src/fonts/**/*', 'src/images/**/*', 'src/scss/**/*.scss'], gulp.series(['js', 'html', 'fonts', 'images', 'sass', "sass-lint"]));
 });
 
--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,27 @@
 /* Here goes your JS code */
+'use strict';
+var ESCAPE_KEY = 'Escape';
+var popup = document.querySelector('.popup');
+var clickMeButton = document.querySelector('#show-popup-form');
+var closePopupButton = document.querySelector('.popup__close');
+
+var hidePopupWithEscape = function (evt) {
+  if (evt.key === ESCAPE_KEY) {
+    hidePopup();
+  }
+};
+
+var showPopup = function () {
+  popup.classList.add('popup-show');
+  document.addEventListener('keydown', hidePopupWithEscape);
+}
+
+var hidePopup = function () {
+  if (popup.classList.contains('popup-show')) {
+    popup.classList.remove('popup-show');
+  }
+  document.removeEventListener('keydown', hidePopupWithEscape);
+};
+
+clickMeButton.addEventListener("click", showPopup);
+closePopupButton.addEventListener('click', hidePopup);
