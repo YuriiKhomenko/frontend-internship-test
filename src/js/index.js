afe4846af3af01fb72fe71783184821c7ccc45a9
Yurii Khomenko <yuriikhomenko@macBookProYurii.home>
2020-03-18 21:36:12 +0100

    Adds solutions to Task #2 and Task #3

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,13 @@
 /* Here goes your JS code */
 'use strict';
 var ESCAPE_KEY = 'Escape';
+var SUBMIT_DELAY = 3000;
+
 var popup = document.querySelector('.popup');
 var clickMeButton = document.querySelector('#show-popup-form');
 var closePopupButton = document.querySelector('.popup__close');
+var submitFormButton = document.querySelector('.popup__submit')
+var popupForm = document.querySelector('.popup__form');
 
 var hidePopupWithEscape = function (evt) {
   if (evt.key === ESCAPE_KEY) {
@@ -23,5 +27,21 @@ var hidePopup = function () {
   document.removeEventListener('keydown', hidePopupWithEscape);
 };
 
-clickMeButton.addEventListener("click", showPopup);
+var successMsgShow = function () {
+  clickMeButton.style.display = 'none';
+  var div = document.createElement('div');
+  div.textContent = 'Thank you!';
+  div.classList.add('success-msg');
+  document.querySelector('.main').appendChild(div);
+};
+
+submitFormButton.addEventListener('click', function (evt) {
+  evt.preventDefault();
+  successMsgShow();
+  setTimeout(function () {
+    hidePopup();
+  }, SUBMIT_DELAY);
+});
+
+clickMeButton.addEventListener('click', showPopup);
 closePopupButton.addEventListener('click', hidePopup);
